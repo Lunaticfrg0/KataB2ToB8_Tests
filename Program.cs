33af35297571e3105b9b985a1611a8b68f19b61f
Lunaticfrg0 <emilio3scano@gmail.com>
2021-01-14 18:12:08 -0400

    Official pass of test case 1

--- Program.cs
+++ Program.cs
@@ -7,10 +7,51 @@ namespace Kata__6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(ConvertB10ToB8(42));
+        }
+        public static int ConvertB2ToB10(string binary)
+        {
+            int binaryNumber = int.Parse(binary);
+            int decimalNumber = 0;
+            int base1 = 1;
+
+            while(binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalNumber += reminder * base1;
+                base1 = base1 * 2;
+            }
+
+            return decimalNumber;
+        }
+        public static string ConvertB10ToB8(int base10)
+        {
+            int numToChange = base10;
+            string base8 = "";
+
+            while(true){
+                if((numToChange % 8) != 0)
+                {
+                    base8 = (numToChange % 8).ToString()+ base8;
+                }
+                else
+                {
+                    base8 = "0"+ base8;
+                }
+                numToChange/=8;
+
+                if(numToChange <= 0)
+                {
+                    break;
+                }
+            }
+
+            return base8;
         }
         public static string ConvertB2ToB8(string x)
         {
-            return "71";
+            return ConvertB10ToB8(ConvertB2ToB10(x));
         }
     }
 }
